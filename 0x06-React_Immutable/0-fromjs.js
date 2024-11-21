944037a591939d10ec6b342ca377f130a4402f55
Ezekwe chibuikem <nwachinaemelum@yahoo.com>
2024-11-21 16:59:11 +0100

    fix

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -3,3 +3,13 @@ import { fromJS } from 'immutable';
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
+
+const obj = { 
+  fear: true, 
+  smell: -1033575916.9145899, 
+  wall: false, 
+  thing: -914767132 
+}; 
+
+const immutableObj = getImmutableObject(obj); 
+console.log(immutableObj);
